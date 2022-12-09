8357b001c2fcef1847af99a16401fff4beab6de2
TonTon9 <max.schadrin.2002@yandex.ru>
2022-12-10 00:04:54 +0300

    add character controller, animation and post processing

---
+++ Assets/Scripts/PlayerController/PlayerAnimations.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerAnimations {
+
+    private Animator _animator;
+    
+    public PlayerAnimations(Animator animator) {
+        _animator = animator;
+    }
+    
+    public void SetWalkSpeed(float verticalSpeed ,float horizontalSpeed ) {
+        _animator.SetFloat("Speed", verticalSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : IMove {
+    private CharacterController _charController;
+    private float _speed;
+    private Vector3 _totalDirection;
+    private float _directionX;
+    private float _directionZ;
+    private float _rotationSpeed;
+    
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    public PlayerMovement(CharacterController characterController, float speed) {
+        _charController = characterController;
+        _speed = speed;
+    }
+    
+    public void Move() {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter() {
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+        _directionX = _horizontalInput * _speed * Time.deltaTime;
+        _directionZ = _verticalInput * _speed * Time.deltaTime;
+        _totalDirection = new Vector3(_directionX, 0, _directionZ);
+        _totalDirection = _charController.transform.TransformDirection(_totalDirection);
+        _charController.Move(_totalDirection);
+    }
+
+    public float GetVerticalSpeed() {
+        return _verticalInput;
+    }
+
+    public float GetHorizontalSpeed() {
+        return _horizontalInput;
+    }
+}
